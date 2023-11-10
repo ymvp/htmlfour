290c33e1eb3503600820a34c37d486cac081592c
ymvp <ymvp76@gmail.com>
2023-11-10 12:05:42 +0530

    first commit

---
+++ app4/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def aditi(request):
+    return render(request,'aditi.html')
+
+def dantera(request):
+    return render(request,'dantera.html')
+
+def festivalvibes(request):
+    return render(request,'festivalvibes.html')
+
+def preethi(request):
+    return render(request,'preethi.html')
+
+def varada(request):
+    return render(request,'varada.html')
